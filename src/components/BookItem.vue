56c4d97263a921dc9278678201966e161c97905f
Stoyan Stoyanov <sstoyanov@Level10.com>
2020-06-16 14:02:49 -0500

    commit 6

---
+++ src/components/BookItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    <li>
+        {{book.title}}:{{book.author}}
+    </li>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BookItem',
+       props:{
+           book: {
+             type: string,
+             default: '',
+           }
+       }
+    }
+</script>
+
+<style>
+li{
+     display: block;
+     margin: 0 10px;
+ }
+</style>
